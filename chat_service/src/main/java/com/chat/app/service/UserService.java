6141ac5cd496aacaae47a86c920989d48e3fb043
xulin <xulin@998f5c80-9c9f-4b53-b3cc-e062a4a43f30>
2018-11-12 09:31:51 +0000

    注册、登陆、重置密码

--- chat_service/src/main/java/com/chat/app/service/UserService.java
+++ chat_service/src/main/java/com/chat/app/service/UserService.java
@@ -5,12 +5,14 @@ import com.chat.app.dao.mapper.UserMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.google.common.base.Preconditions;
+import com.us.base.util.MD5Encryption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by DL on 2017/1/3.
@@ -47,7 +49,32 @@ public class UserService {
             return true;
         return false;
     }
-    
+
+    /**
+     * 根据账号查询用户信息
+     * @param userCode
+     * @return
+     */
+    public User findUsertByUserCode(String userCode) {
+
+        User userInfo = new User();
+        userInfo.setUserCode(userCode);
+        User user = userMapper.selectOne(userInfo);
+        return user;
+    }
+
+    /**
+     * 根据手机号查询用户信息
+     * @param telephone
+     * @return
+     */
+    public User findUsertByTelephone(String telephone) {
+
+        User userInfo = new User();
+        userInfo.setTelephone(telephone);
+        User user = userMapper.selectOne(userInfo);
+        return user;
+    }
 
     /**
      * 手机号注册
@@ -57,14 +84,48 @@ public class UserService {
      * @throws Exception
      */
     public  User registeUser(String telephone, String password) throws Exception {
+
         User user = new User();
         user.setTelephone(telephone);
-        user.setUserPassword(password);
+        user.setPassword(password);
         user.setCreateTime(new Date());
         user.setEnable(1);
+        user.setUserCode(createUserCode());
         userMapper.insert(user);
         return  user;
     }
+
+    /**
+     * 更新用户信息
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    public  int updateUserInfo(User user) throws Exception {
+
+        Preconditions.checkNotNull(user, "用户数据为空");
+        return userMapper.updateByPrimaryKeySelective(user);
+    }
+
+
+
+    /**
+     * 创建账号
+     * @return
+     * @throws Exception
+     */
+    private  String createUserCode() throws Exception {
+
+        String userCode= String.valueOf(new Random().nextInt(100000 ))+String.valueOf(new Random().nextInt(10000 ));
+        User userInfo = new User();
+        userInfo.setUserCode(userCode);
+        if (userMapper.selectCount(userInfo) > 0 ){
+            createUserCode();
+        }
+        return  userCode;
+    }
+
+
 
 
 }
--- chat_service/src/main/java/com/chat/app/web/controller/UserController.java
+++ chat_service/src/main/java/com/chat/app/web/controller/UserController.java
@@ -2,28 +2,43 @@ package com.chat.app.web.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.chat.app.dao.beans.User;
+import com.chat.app.dao.mapper.UserMapper;
+import com.chat.app.service.LoginService;
 import com.chat.app.service.UserService;
 import com.github.pagehelper.PageInfo;
 import com.google.common.base.Preconditions;
 import com.us.base.redis.RedisCommon;
+import com.us.base.util.MD5Encryption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
- * Created by zbs on 2016/12/14.
+ * Created by xulin on 2018/11/12.
  */
 @RestController
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
     @Autowired
+    @Qualifier(value = "userService")
     private UserService userService;
+
+    @Autowired
+    @Qualifier(value = "loginService")
+    private LoginService loginService;
+
+    @Autowired
+    @Qualifier(value = "userMapper")
+    private UserMapper userMapper;
 
 
 
@@ -34,17 +49,103 @@ public class UserController extends BaseController {
         return ResponseWrapper().addData(userList).ExeSuccess();
     }
 
-
-    @RequestMapping(value = "addUser", method = RequestMethod.POST)
+    /**
+     *用户注册接口
+     * @param request
+     * @param telephone
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "userRegister", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject createActivity(
+    public JSONObject userRegister(
             HttpServletRequest request,
-            @ModelAttribute User user
+            @RequestParam(value = "telephone") String telephone,
+            @RequestParam(value = "password") String password
+    ) throws Exception {
+        Preconditions.checkState(!userService.isExistByTelephone(telephone),"该手机号已经注册,请前往登陆");
+        User user= userService.registeUser(telephone, MD5Encryption.getMD5(password));
+        return ResponseWrapper().addData(user).addMessage("操作成功").ExeSuccess();
+    }
+
+    /**
+     * 判断手机号是否注册
+     * @param request
+     * @param telephone
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "isExistByTelephone", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject isExistByTelephone(
+            HttpServletRequest request,
+            @RequestParam(value = "telephone") String telephone
     ) throws Exception {
 
-    RedisCommon.push("xulin","111111");
-        return ResponseWrapper().addMessage("操作成功").ExeSuccess();
+       Boolean result= userService.isExistByTelephone(telephone);
+        return ResponseWrapper().addData(result).ExeSuccess();
     }
+
+
+
+    @RequestMapping(value = "loginByUserCode", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject loginByUserCode(
+            HttpServletRequest request,
+            @RequestParam(value = "userCode") String userCode,
+            @RequestParam(value = "password") String password
+    ) throws Exception {
+        User user = userService.findUsertByUserCode(userCode);
+        //判断用户是否存在
+        Preconditions.checkState(user!=null,"该账号号尚未注册,请前往注册");
+        //判断账号和密码是否正确
+        password = MD5Encryption.getMD5(password);
+        Preconditions.checkState(password.equals(user.getPassword()), "账号或密码错误,请重新登录");
+        //执行登陆操作
+        String token = loginService.login(user.getId());
+        //返回用户基本信息和token
+        Map<String,Object> map = new HashMap<>();
+        map.put("token",token);
+        map.put("user",user);
+        return ResponseWrapper().addData(map).ExeSuccess();
+    }
+
+    /**
+     * 重置密码
+     * @param request
+     * @param telephone
+     * @param newpassword
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "resetPassword", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject resetPassword(
+            HttpServletRequest request,
+            @RequestParam(value = "telephone") String telephone,
+            @RequestParam(value = "newpassword") String newpassword
+    ) throws Exception {
+        //判断用户是否存在
+        Preconditions.checkState(userService.isExistByTelephone(telephone),"该手机号尚未注册,请前往注册");
+        //执行用户重置密码操作
+        User param = userService.findUsertByTelephone(telephone);
+        User user = new User();
+        user.setId(param.getId());
+        user.setPassword(MD5Encryption.getMD5(newpassword));
+        userService.updateUserInfo(user);
+        //执行登陆，并返回用户基本信息
+        String token = loginService.login(param.getId());
+        User userInfo= userMapper.selectByPrimaryKey(param.getId());
+        Map<String,Object> map = new HashMap<>();
+        map.put("token",token);
+        map.put("user",userInfo);
+        return ResponseWrapper().addData(map).addMessage("密码设置成功").ExeSuccess();
+    }
+
+
+
+
 
     @RequestMapping(value = "testRedis", method = RequestMethod.POST)
     @ResponseBody
